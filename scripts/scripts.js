e2600dad6967fd86532b0588c0cdafa6466c8092
mohamedabusrea <mohamedabusrea@gmail.com>
2020-04-26 00:02:34 +0200

    add some fixes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,15 @@
 (function () {
     const remainingTimeElement = document.querySelector('.container__remainingTime');
     const todayGeorgianDate = moment();
-    const thisHijriYear = parseInt(todayGeorgianDate.format('iYYYY'));
     const todayHijriFormat = todayGeorgianDate.format('iYYYY/iM/iD');
     const todayHijriDate = moment(todayHijriFormat, 'iYYYY/iM/iD');
+    const thisHijriYear = parseInt(todayGeorgianDate.format('iYYYY'));
     let ramadanDate = moment(`${thisHijriYear}/9/1`, 'iYYYY/iM/iD');
     let daysInRamadan = ramadanDate.daysInMonth();
     let diff = todayHijriDate.diff(ramadanDate, 'days');
 
     if (diff >= 0) {
-        if (diff <= 30) {//during Ramadan
+        if (diff <= daysInRamadan) {//during Ramadan
             document.querySelector('.container')
                     .classList
                     .add('container--duringRamadan');
